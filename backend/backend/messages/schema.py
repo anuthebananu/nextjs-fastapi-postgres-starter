7716a90b78883263ef2cb491fc52984bbd29ca71
anuthebananu <amanjaria@gmail.com>
2024-09-25 19:52:34 -0400

    message response schema

--- backend/backend/messages/schema.py
+++ backend/backend/messages/schema.py
@@ -1,7 +1,28 @@
+from datetime import datetime
+from typing import Optional
 from uuid import UUID
 from pydantic import BaseModel
 
 class CreateMessageRequest(BaseModel):
-    conversation_id: UUID
+    user_id: int
     text: str
-    
+    conversation_id: Optional[UUID] = None
+
+# Can be augmented in future w/ image url, video url, metadata, etc
+class ContentResponse(BaseModel):
+    id: UUID
+    text: str
+
+class MessageResponse(BaseModel):
+    id: UUID
+    receivedAt: datetime
+    processedAt: datetime
+    status: str
+    sender_id: int
+    content: ContentResponse
+    conversation_id: UUID
+
+# TODO pagination
+class MessagesResponse(BaseModel):
+    data: list[MessageResponse]
+
