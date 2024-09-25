db725209b908f081b76e018921893607f75e3974
anuthebananu <amanjaria@gmail.com>
2024-09-25 19:52:06 -0400

    add model pk defaults

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import DateTime, ForeignKey, String
-from uuid import UUID
+from uuid import UUID, uuid4
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -19,29 +19,37 @@ class User(Base):
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}"
 
+class Content(Base):
+    __tablename__ = "contents"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4())
+    text: Mapped[str] = mapped_column(String)
+
+    def __repr__(self) -> str:
+        return f"Content(id={self.id!r}, text={self.text!r})"
+
 class Conversation(Base):
     __tablename__ = "conversations"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True)
-    message_id: Mapped[UUID] = mapped_column(ForeignKey("messages.id"))
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4())
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Conversation(id={self.id!r}, owner_id={self.owner_id!r})"
 
 class Response(Base):
     __tablename__ = "responses"
-    id: Mapped[UUID] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4())
     sent_at: Mapped[datetime] = mapped_column(DateTime)
-    content_id: Mapped[UUID] = mapped_column(ForeignKey("content.id"))
+    content_id: Mapped[UUID] = mapped_column(ForeignKey("contents.id"))
 
 class Message(Base):
     __tablename__ = "messages"
-    id: Mapped[UUID] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4())
     received_at: Mapped[datetime] = mapped_column(DateTime)
     processed_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     status: Mapped[str] = mapped_column(String)
     sender_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    content_id: Mapped[UUID] = mapped_column(ForeignKey("content.id"))
+    content_id: Mapped[UUID] = mapped_column(ForeignKey("contents.id"))
+    conversation_id: Mapped[UUID] = mapped_column(ForeignKey("conversations.id"))
 
-class Content(Base):
-    __tablename__ = "contents"
-    id: Mapped[UUID] = mapped_column(primary_key=True)
-    text: Mapped[str] = mapped_column(String)
 
